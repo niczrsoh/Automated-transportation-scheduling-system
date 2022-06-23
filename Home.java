b236cca3af84d0d3fd0d87ae87f39f4f71500ac1
niczrsoh <87169349+niczrsoh@users.noreply.github.com>
2022-06-23 18:45:09 +0800

    adding page

---
+++ Home.java
@@ -0,0 +1,15 @@
+/*
+Automated transport scheduling system
+1. Soh Zen Ren A20EC0152
+2. Wong Hui Shi A20EC0169
+3. Teoh Wei Jian A20EC0229
+ */
+//Home Page!!
+public class Home {
+    public static void main(String[] args){
+        int choice;
+        //Making menu and selection 
+
+ 
+    }
+}
